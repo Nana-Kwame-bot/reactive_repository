8b430e5c1fc51adb1908cf691d773b30488f29ed
Nana Kwame <aduhenry9@gmail.com>
2024-05-02 01:52:22 +0000

    chore: Update {{bloc_state_name.pascalCase()}}Repository to include stream and update methods

--- __brick__/{{bloc_state_name.snakeCase()}}_repository.dart
+++ __brick__/{{bloc_state_name.snakeCase()}}_repository.dart
@@ -1,7 +1,12 @@
 import 'package:rxdart/rxdart.dart';
 
+/// A repository class for [{{bloc_state_name}}].
+///
+/// This class provides a way to manage and update the state of [{{bloc_state_name}}].
 class {{bloc_state_name.pascalCase()}}Repository {
-
+  /// A [BehaviorSubject] to hold and stream the [{{bloc_state_name}}].
+  ///
+  /// It's seeded with an initial state if seedBehaviorSubject is true.
   final BehaviorSubject<{{bloc_state_name}}> _{{bloc_state_name.camelCase()}}Subject =
     {{#seedBehaviorSubject}}
     BehaviorSubject<{{bloc_state_name}}>.seeded(const {{bloc_state_name}}());
@@ -9,15 +14,22 @@ class {{bloc_state_name.pascalCase()}}Repository {
     {{^seedBehaviorSubject}}BehaviorSubject<{{bloc_state_name}}>();
     {{/seedBehaviorSubject}}
 
+  /// A stream of [{{bloc_state_name}}] updates for subscription.
   Stream<{{bloc_state_name}}> get {{bloc_state_name.camelCase()}}Stream =>
     _{{bloc_state_name.camelCase()}}Subject.stream;
 
+  /// Provides synchronous access to the current state.
   {{bloc_state_name}} get current{{bloc_state_name}} => _{{bloc_state_name.camelCase()}}Subject.value;
 
+  /// Allows for the [{{bloc_state_name}}] to be updated, notifying all 
+  /// stream subscribers.
+  ///
+  /// The [state] is the new state to be added to the stream.
   void update{{bloc_state_name}}({{bloc_state_name}} state) =>
     _{{bloc_state_name.camelCase()}}Subject.add(state);
 
   {{#includeDispose}}
+  /// Closes the [BehaviorSubject] stream to prevent memory leaks upon disposal.
   void dispose() => _{{bloc_state_name.camelCase()}}Subject.close();
   {{/includeDispose}}
 }
